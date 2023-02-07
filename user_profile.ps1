6c89f240a0542adbb1a14b0d6d6bf89fe8939fc5
Robin Granberg <robing@semperis.com>
2023-02-07 13:09:26 +0100

    Set-Alias Scope Global

--- user_profile.ps1
+++ user_profile.ps1
@@ -16,8 +16,8 @@ $PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'canix.omp.json'
 oh-my-posh init pwsh --config $PROMPT_CONFIG | Invoke-Expression
 
 # Alias
-Set-Alias vim nvim
-Set-Alias ll ls
-Set-Alias grep findstr
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias vim nvim -scope global
+Set-Alias ll ls -scope global
+Set-Alias grep findstr -scope global
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe' -scope global
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe' -scope global
